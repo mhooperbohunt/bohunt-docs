1d43cc9b1dbbc09ed3ab0d9a31a25ef898422c28
mac <macdevh@gmail.com>
2020-08-20 13:48:42 +0100

    updated links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,13 +86,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/mhooperbohunt/bohunt-docs/master/website/',
+            'https://github.com/mhooperbohunt/bohunt-docs/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/mhooperbohunt/bohunt-docs/master/website/blog/',
+            'https://github.com/mhooperbohunt/bohunt-docs/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
